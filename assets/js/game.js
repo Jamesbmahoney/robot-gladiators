dbe8933683677af3c7ef953985978a65a018c5f2
James Barry <jbm03003@gmail.com>
2021-11-30 19:01:08 -0500

    added array for enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+//"WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack= 10;
@@ -6,13 +12,14 @@ var playerMoney = 10;
 //You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    window.alert("Welcome to Robot Gladiators!");    
+    }
 
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         if (promptFight === "fight"  || promptFight === "FIGHT") {
@@ -32,7 +39,7 @@ var fight = function() {
         //remove player's health by subtracting the amout set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         //check player's health
